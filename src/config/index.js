e5574c348c023bc565bc7cd44e395fd57a947353
BrandBeru <brand.beru@gmail.com>
2025-02-05 11:57:30 -0500

    adding proxy settings

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,8 @@ const config = {
     project: process.env.PROJECT,
     spreadsheet_id: process.env.SPREADSHEET_ID,
     private_key: process.env.PRIVATE_KEY,
+    crm_url: process.env.CRM_URL,
+    crm_api_key: process.env.CRM_API_KEY,
 }
 
 export default config
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from "./config/index.js";
 import routerApi from "./routes/index.js";
 import cors from "cors";
+import proxy from "./routes/proxy.js";
 
 const port = config.port
 
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+proxy(app)
 routerApi(app)
 
 app.listen(port, () => {
